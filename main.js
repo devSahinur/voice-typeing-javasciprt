49e8dc86419b68e85947012a720b55426cc934e4
Sahinur Islam <infosahinur@gmail.com>
2021-12-13 23:00:10 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+let results = document.getElementById("results");
+let interim = document.getElementById("interim");
+
+let final_transcript = "";
+let interim_transcript = "";
+
+let recognition = new webkitSpeechRecognition();
+recognition.continuous = true;
+recognition.interimResults = false;
+
+let start = function(){
+    results.innerHTML = '';
+    interim.innerHTML = '';
+    final_transcript = '';
+    interim_transcript = '';
+    recognition.start();
+
+    recognition.onresult = function(event){
+        interim_transcript = '';
+
+        for(let i = event.resultIndex; i < event.results.length; ++i)
+        {
+            if(event.results[i].isFinal){
+                final_transcript += event.results[i][0].transcript;
+            }else{
+                interim_transcript += event.results[i][0].transcript;
+            }
+        } 
+        results.innerHTML = final_transcript;
+        interim.innerHTML = "currently Saying" + interim_transcript;
+    };
+};
+
+let stop = function(){
+    recognition.stop();
+}
