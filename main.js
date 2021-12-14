14c86095aa83898001a23fea3a8d88d8b698e823
Sahinur Islam <infosahinur@gmail.com>
2021-12-14 10:04:39 +0600

    code format and update

--- main.js
+++ main.js
@@ -8,29 +8,28 @@ let recognition = new webkitSpeechRecognition();
 recognition.continuous = true;
 recognition.interimResults = false;
 
-let start = function(){
-    results.innerHTML = '';
-    interim.innerHTML = '';
-    final_transcript = '';
-    interim_transcript = '';
-    recognition.start();
+let start = function () {
+  results.innerHTML = "";
+  interim.innerHTML = "";
+  final_transcript = "";
+  interim_transcript = "";
+  recognition.start();
 
-    recognition.onresult = function(event){
-        interim_transcript = '';
+  recognition.onresult = function (event) {
+    interim_transcript = "";
 
-        for(let i = event.resultIndex; i < event.results.length; ++i)
-        {
-            if(event.results[i].isFinal){
-                final_transcript += event.results[i][0].transcript;
-            }else{
-                interim_transcript += event.results[i][0].transcript;
-            }
-        } 
-        results.innerHTML = final_transcript;
-        interim.innerHTML = "currently Saying" + interim_transcript;
-    };
+    for (let i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+        final_transcript += event.results[i][0].transcript;
+      } else {
+        interim_transcript += event.results[i][0].transcript;
+      }
+    }
+    results.innerHTML = final_transcript;
+    interim.innerHTML = "currently Saying" + interim_transcript;
+  };
 };
 
-let stop = function(){
-    recognition.stop();
-}
+let stop = function () {
+  recognition.stop();
+};
